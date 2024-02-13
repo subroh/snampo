671f11be2291fa17ebde0aa31e60e4f33468a58e
KineYue <134162137+KineYue@users.noreply.github.com>
2024-02-13 20:06:55 +0900

    [feature] ios版の対応
    Google map のAPI
    geolocatorへの対応
    image_pickerへの対応

---
+++ frontend/ios/Runner/Environment.swift
@@ -0,0 +1,12 @@
+//
+//  Environment.swift
+//  Runner
+//
+//  Created by 関根悠司 on 2024/02/13.
+//
+
+import Foundation
+
+struct Env {
+ static let googleMapApiKey = "AIzaSyA2DTkT-sRE4sQhUqeYEECxyDcR4LIUQE4"
+}
